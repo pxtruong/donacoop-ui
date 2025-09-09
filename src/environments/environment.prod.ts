6c415c44f440f959ff0c840f848bbc8e02cdd662
Tri <phamminhtri26102003@gmail.com>
2025-09-09 10:43:17 +0700

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: '/api/',
+  apiURL: 'https://donacoop-be-1wgy.onrender.com/api/v1',
   logLevel: 1, // Set the log level to error for production
 };
