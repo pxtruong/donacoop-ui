17aa83410f9acc1cd52d1205b3ad75681dc14e6e
Tri <phamminhtri26102003@gmail.com>
2025-09-13 20:18:48 +0700

    update env ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: 'https://donacoop-be-umiv.onrender.com/api/v1',
-  logLevel: 4, // Set the log level to error for production
+  logLevel: 1, // Set the log level to error for production
 };
