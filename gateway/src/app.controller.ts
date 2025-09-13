201a249da73f9336653e131c770e27c41f9c583d
Tri <phamminhtri26102003@gmail.com>
2025-09-13 17:48:26 +0700

    add gateway

--- gateway/src/app.controller.ts
+++ gateway/src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('heathy')
+  getHello() {
+    const buildVersion = this.configService.get('BUILD_VERSION');
+    if (buildVersion) {
+      return { buildVersion: `${buildVersion}` };
+    }
+    return 'Page Not Found';
   }
 }
